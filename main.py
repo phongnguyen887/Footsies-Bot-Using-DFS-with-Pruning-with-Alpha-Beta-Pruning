6f9ce0542b3ac0c2d3b9d3d7a9fc667a5dbf3bd2
Alex-R-Projects <mralexreyes99@gmail.com>
2024-11-20 17:44:58 -0800

    + Added the keyboard library

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess # This library would be used to open the game FOOTSIES.exe
 import time # This would be used to delay keyboard presses and wait for the game to launch
 from pymem  import Pymem # This library allows for the manipulation for processes in Windows
-
+import keyboard # This library will allows the program to send keybaord inputs to the game
 # We can use these two libraries to force the installation of the necessary libraries
 import os
 import sys
